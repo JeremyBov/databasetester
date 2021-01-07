7b8774f2febf7b2dc6b300679e2ab23950ec9fbc
Jeremy <jeremy.bovenkamp@gmail.com>
2021-01-07 23:07:00 +0100

    Add project files.

---
+++ WebApplication3/Controllers/CalcController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class CalcController : Controller
+    {
+        // GET: Calc
+        public ActionResult Index()
+        {
+            return View(new calc());
+        }
+        [HttpPost]
+        public ActionResult Index(calc c,string calculate)
+        {
+
+            if (calculate == "add") 
+            {
+                c.tot = c.no1 + c.no2;
+            }
+            else if (calculate == "min")
+            {
+                c.tot = c.no1 - c.no2;
+            }
+            else if (calculate == "sub")
+            {
+                c.tot = c.no1 * c.no2;
+            }
+            else if (calculate == "divv")
+            {
+                c.tot = c.no1 / c.no2;
+            }
+            else if (calculate == "equals")
+            {
+                int x = 0;
+                Int32.TryParse(c.area, out x);
+                int x2 = Int32.Parse(c.area);
+                c.area = (x).ToString();
+                return View(c);
+            }
+
+
+
+
+            if (c.tot == 69) 
+            { c.truetotal = "nice";
+                return View(c);
+            }
+            if (c.tot == 420)
+            {
+                c.truetotal = "smoke weed everyday";
+                return View(c);
+            }
+            if (c.tot == 34)
+            {
+                c.truetotal = "appie noerie";
+                return View(c);
+            }
+            c.truetotal = (c.tot).ToString();
+            return View(c);
+
+
+        }
+    }
+}
---
+++ WebApplication3/Models/calc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class calc
+    {
+        public int no1 { get; set; }
+        public int no2 { get; set; }
+
+        public int tot { get; set; }
+
+        public string truetotal { get; set; }
+
+        public string area { get; set; }
+    }
+}
